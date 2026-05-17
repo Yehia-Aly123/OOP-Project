8bddc3e138b0109ba6b54a18f664cee1f631ce58
Yehia-Aly123 <yehia.alyagm@gmail.com>
2026-05-17 19:24:54 +0300

    Added Person and Player base models

--- Project/src/main/java/footsys/project/model/Manager.java
+++ Project/src/main/java/footsys/project/model/Manager.java
@@ -1,4 +1,21 @@
 package footsys.project.model;
 
-public class Manager {
+public class Manager extends Person {
+    String Tactics;
+    int Wins, Draws, Losses;
+    public Manager(String name, int age, double wagepw, double bonus, String Tactics, int Wins, int Draws, int Losses)
+    {
+        super(name,age,wagepw,bonus);
+        this.Tactics = Tactics;
+        this.Wins = Wins;
+        this.Draws = Draws;
+        this.Losses = Losses;
+    }
+    public double wage()
+    {
+        return wagepw + bonus;
+    }
+    public String getdetails() {
+        return super.getdetails() + "Tactic: "+ Tactics +" | Wins:  "+ Wins+" | Draws:  " + Draws+" | Losses:  " + Losses;
+    }
 }
--- Project/src/main/java/footsys/project/model/Player.java
+++ Project/src/main/java/footsys/project/model/Player.java
@@ -3,14 +3,14 @@ package footsys.project.model;
 public class Player extends Person {
     int Shirtnumber;
     String position;
-    public Player(String name,int age,double wagepw,int Shirtnumber, String position, double bonus)
+    public Player(String name,int age,double wagepw, double bonus,int Shirtnumber, String position)
     {
         super(name, age, wagepw, bonus);
         this.Shirtnumber=Shirtnumber;
         this.position=position;
     }
     public double wage() {
-        return wagepw;
+        return wagepw + bonus;
     }
     public String getdetails() {
         return super.getdetails() + "Shirt Number: "+Shirtnumber+" | Position: "+position;
